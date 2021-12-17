0c8714e64fecaf81f82354c643d56c25322ef316
Guptaverse <sggreat88@gmail.com>
2021-12-18 00:24:35 +0530

    updated with insertion at middle and end

--- insertion.cpp
+++ insertion.cpp
@@ -24,6 +24,38 @@ void insertAthead(node*&head,int d){   //passing head by reference
     n->next = head;
     head = n;
 }
+void insertAtMiddle(node*&head,int d,int pos){
+    if(head==NULL){
+        head = new node(d);
+        return;
+    }
+    node*temp =head;
+    int idx =1;
+    while(temp!=NULL){
+        node*n = new node(d);
+        if(idx == pos-1){
+            n->next = temp->next;
+            temp->next = n;
+        }
+        idx++;
+        temp = temp->next;
+    }
+    temp = head;
+
+}
+void insertAtEnd(node*&head,int d){
+    if(head==NULL){
+        head = new node(d);
+        return;
+    }
+    node*temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    node*n = new node(d);
+    temp->next=n;
+    temp=head;
+}
 void printList(node*head){    //passing head by value
     while(head->next!=NULL){
         cout<<head->data<<"->";
@@ -38,6 +70,10 @@ int main(){
     insertAthead(head,3);
     insertAthead(head,2);
     insertAthead(head,1);
+    printList(head);
+    insertAtMiddle(head,5,3);
+    printList(head);
+    insertAtEnd(head,8);
     printList(head);
     return 0;
 }
